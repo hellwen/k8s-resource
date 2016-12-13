3a7e246e74987d3d4e52f78e5a2e82b6f74e15c0
Hellwen <hellwen.wu@gmail.com>
2016-12-13 15:01:59 +0800

    ok

--- kube-config/registry/generate-certs.sh
+++ kube-config/registry/generate-certs.sh
@@ -6,3 +6,4 @@ rm -rf certs/*
 DOMAIN="k8s-registry.zeusis.com"
 
 openssl req -newkey rsa:2048 -nodes -sha256 -keyout certs/domain-key -subj "/CN=${DOMAIN}" -x509 -days 365 -out certs/domain-crt
+cp certs/domain-crt certs/ca.crt
